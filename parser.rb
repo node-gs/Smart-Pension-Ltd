5a3922487368192e5dce0d774e97c99ebdb33a17
node-gs <gardesimpson@gmail.com>
2020-01-15 13:51:41 +0000

    parser can now collate and process views

--- parser.rb
+++ parser.rb
@@ -3,8 +3,27 @@ class Parser
     @log = log
   end
 
-  def read_file
+  def read_file 
     File.read(@log).split("\n")
   end
 
+  def process_formatted_views 
+    formatted_views = read_file.map do |view|
+      current_endpoint = view.split.first
+      {
+        endpoint: current_endpoint,
+        ips: calculate_ips(current_endpoint),
+        visits: calculate_ips(current_endpoint).size,
+        unique_visits: calculate_ips(current_endpoint).uniq.size
+      }
+    end
+    formatted_views.uniq { |view| view[:endpoint]} 
+  end
+
+private
+  def calculate_ips current_endpoint
+    read_file
+      .map { |view| view.split[1] if current_endpoint == view.split.first }
+      .compact
+  end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,9 +1,30 @@
 require './parser'
-
 describe Parser do
+  formatted_views = ["/help_page/1 184.123.665.067", "/help_page/1 184.123.665.067", "/contact 184.123.665.067", "/help_page/1 126.318.035.038"]
+  formatted_data_structure = [
+    {
+      endpoint: "/help_page/1",
+      ips: ["184.123.665.067", "184.123.665.067", "126.318.035.038"],
+      unique_visits: 2,
+      visits: 3
+    },
+    {
+      endpoint: "/contact",
+      ips: ["184.123.665.067"],
+      unique_visits: 1,
+      visits: 1
+    }
+  ]
+  
   it 'can read a file' do
     expect(File).to receive(:read).and_return("line one \n line two")
 
     described_class.new(log: 'some string').read_file
+  end
+
+  it 'can process a file into an appropriate data structure' do
+    parser = described_class.new(log: 'some string')
+    allow(parser).to receive(:read_file).and_return(formatted_views)
+    expect(parser.process_formatted_views).to eq formatted_data_structure 
   end
 end
